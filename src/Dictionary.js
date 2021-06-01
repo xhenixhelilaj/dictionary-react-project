065bfd1daa9e1a6f082653c1813df424f6ce7ef6
Xheni Xhelilaj <73161981+xhenixhelilaj@users.noreply.github.com>
2021-06-01 20:34:10 +0200

    added new state and two new components

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,20 +1,31 @@
 import React,{useState} from "react"
+import Results from "./Results"
+import axios from "axios"
 
 export default function Dictionary(){
     let [keyword, setKeyword] = useState("")
+    let [results, setResults]=useState(null)
     function search(event){
         event.preventDefault()
-        alert(`Searching for ${keyword}`)
     }
     function handleChange(event){
         setKeyword(event.target.value)
     }
+    function handleResponse (response){
+        setResults(response.data[0])
+    }
+
+    let apiUrl= `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`
+    axios.get(apiUrl).then(handleResponse)
+    
+
     return(
         <div>
             <form onSubmit={search}>
                 <input type="search" placeholder="enter a term" onChange={handleChange} />
                 <input type="submit" value="🔍" />
             </form>
+            <Results results={results}/>
         </div>
     )
 }
