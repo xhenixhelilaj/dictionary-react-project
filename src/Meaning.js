abb0331ffbb5c8c1cd58c2bdad83b1960c2b0f38
Xheni Xhelilaj <73161981+xhenixhelilaj@users.noreply.github.com>
2021-06-02 09:55:51 +0200

    spacing

--- src/Meaning.js
+++ src/Meaning.js
@@ -11,10 +11,10 @@ export default function Meaning(props){
                 return (
                     <div key={index}>
                         <p>
-                          <strong>Definition:</strong>
+                          <strong>Definition: </strong>
                           {definition.definition}
                           <br />
-                          <strong>Example:</strong>
+                          <strong>Example: </strong>
                           <em>{definition.example}</em>  
                           <br />
                           <strong><Synonyms synonyms={definition.synonyms}/></strong>
