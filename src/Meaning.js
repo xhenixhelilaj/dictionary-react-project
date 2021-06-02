e35e309bddc89ea92307e6c57d018d9202bfddb2
Xheni Xhelilaj <73161981+xhenixhelilaj@users.noreply.github.com>
2021-06-02 09:28:20 +0200

    new element for the dictionary

--- src/Meaning.js
+++ src/Meaning.js
@@ -13,6 +13,7 @@ export default function Meaning(props){
                           {definition.definition}
                           <br />
                           <em>{definition.example}</em>  
+                          {definition.synonyms}
                         </p>
                     </div>
                 )
