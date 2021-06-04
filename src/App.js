f034167c935d9063c7226f62e3aef681452a65ca
Xheni Xhelilaj <73161981+xhenixhelilaj@users.noreply.github.com>
2021-06-04 19:29:34 +0200

    backround color

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
      <div className="container">
-       <h1>DICTIONARY</h1>
+       <h1>what are you looking for?</h1>
        <Dictionary />
      </div>
      <footer><a href="https://github.com/xhenixhelilaj/dictionary-react-project" rel="noreferrer" target="_blank">Open-sourced</a> on Github</footer>
