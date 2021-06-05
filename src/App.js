484a68bf352508d3238ee16805e6c37e379bd09b
Xheni Xhelilaj <73161981+xhenixhelilaj@users.noreply.github.com>
2021-06-05 14:35:50 +0200

    little changes

--- src/App.js
+++ src/App.js
@@ -5,10 +5,8 @@ function App() {
   return (
     <div className="App">
      <div className="container">
-       <h1>what are you looking for?</h1>
        <Dictionary />
      </div>
-     <footer><a href="https://github.com/xhenixhelilaj/dictionary-react-project" rel="noreferrer" target="_blank">Open-sourced</a> on Github</footer>
     </div>
   );
 }
--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
 import Results from "./Results";
+import "./Dictionary.css"
 import axios from "axios";
-import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
   let [results, setResults] = useState(null);
-
+  let [loaded, setLoaded] = useState(false);
   function search(event) {
     event.preventDefault();
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
@@ -17,29 +17,40 @@ export default function Dictionary() {
   }
   function handleResponse(response) {
     setResults(response.data[0]);
+    setLoaded(true)
   }
 
-  let form = <form onSubmit={search}>
-        <div className="row">
-          <div className="col-9"><input
-          type="search"
-          placeholder="enter a term"
-          onChange={handleChange}
-          className="form-control"
-        /></div>
-          <div className="col-3">
-            <input type="submit" value="🔍" className="btn btn-light" />
-          </div>
-        </div>
-      </form>
-
-
-     return (
-    <div className="name">
-      {form}  
-      <Results results={results} />
+let form =
+  <form onSubmit={search}>
+<div className="row">
+  <div className="col-7"><input
+  type="search"
+  placeholder="enter a term"
+  onChange={handleChange}
+  className="form-control"
+/></div>
+  <div className="col-2">
+    <input type="submit" value="🔍" className="btn btn-light" />
+  </div>
+</div>
+</form>
+if(loaded){
+  return (
+    <div>
+      <h1>what are you looking for?</h1> 
+      {form}
+       <Results results={results} />
+      <footer><a href="https://github.com/xhenixhelilaj/dictionary-react-project" rel="noreferrer" target="_blank">Open-sourced</a> on Github</footer>
     </div>
   );
-  
+}else{
+  return(
+    <div className="center">
+      <h1>what are you looking for?</h1> 
+    {form}
+    <footer><a href="https://github.com/xhenixhelilaj/dictionary-react-project" rel="noreferrer" target="_blank">Open-sourced</a> on Github</footer>
+</div>
+  )
+}
   
 }
