d7870b50bb9ea3ed4edd63b763dc74eb87ef37c1
Xheni Xhelilaj <73161981+xhenixhelilaj@users.noreply.github.com>
2021-06-06 18:35:26 +0200

    alt element

--- src/Photos.js
+++ src/Photos.js
@@ -11,7 +11,7 @@ export default function Photos(props){
                 return (
                     <div className="col-4" key={index}>
                         <a href={photo.src.original} target="_blank" rel="noreferrer" >
-                            <img src={photo.src.landscape} className="img-fluid"/>
+                            <img alt="/" src={photo.src.landscape} className="img-fluid"/>
                         </a>
                    </div>
                 )
