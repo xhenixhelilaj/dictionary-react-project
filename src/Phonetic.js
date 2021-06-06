bdcd22815b8cda8e9dd874ca79cec5a2bad39ccb
Xheni Xhelilaj <73161981+xhenixhelilaj@users.noreply.github.com>
2021-06-06 18:57:38 +0200

    added new react element

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -2,11 +2,15 @@ import React from "react"
 import "./Phonetic.css"
 
 export default function Phonetic(props){
-    console.log(props.phonetic)
-    return (
-        <div className="Phonetic">
-            <a href={props.phonetic.audio} target="_blank"  rel="noreferrer">Listen</a>
-            <span className="text">{props.phonetic.text}</span>
-        </div>
-    )
+ const audio = new Audio(props.phonetic.audio);
+ function playSound(){
+     audio.play();
+ }
+ return(
+     <div className="Phonetic">
+         <i className="fas fa-volume-up" onClick={playSound}></i>
+         <span className="text">{props.phonetic.text}</span>
+     </div>
+ )
+
 }
