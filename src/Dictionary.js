143ad4e688bfaf39dd9751eeae35e35904ab3e71
Xheni Xhelilaj <73161981+xhenixhelilaj@users.noreply.github.com>
2021-06-08 19:30:38 +0200

    logo changed size

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -45,8 +45,8 @@ if(loaded){
   return (
     
     <div className="response">
-      
-      <img src={logo} alt="Logo" className="logo"/>
+      <a href="center">      <img src={logo} alt="Logo" className="logo"/>
+</a>
       <h1>What are you looking for?</h1> 
       {form}
       <div className="results">
@@ -59,7 +59,7 @@ if(loaded){
 }else{
   return(
     <div>
-      <img src={logo} alt="Logo" className="logo" />;
+      <img src={logo} alt="Logo" className="logo"/>;
       <div className="center">
         <div className="first">
           <h1><strong>What are you looking for?</strong></h1>
